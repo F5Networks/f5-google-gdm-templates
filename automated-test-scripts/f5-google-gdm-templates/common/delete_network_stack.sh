a19825b975b7f6f98edcc54622a4d6f2cf1cfa75
kashcheev <a.kashcheev@f5.com>
2021-10-12 21:02:52 +0000

    Release commited to v3.14.0 tag

--- automated-test-scripts/f5-google-gdm-templates/common/delete_network_stack.sh
+++
@@ -1,6 +0,0 @@
-#  expectValue = "completed successfully"
-#  scriptTimeout = 10
-#  replayEnabled = false
-#  replayTimeout = 0
-
-gcloud deployment-manager deployments delete network-<STACK NAME> -q
--- automated-test-scripts/f5-google-gdm-templates/experimental/autoscale/scale_out.sh
+++
@@ -1,8 +0,0 @@
-#  expectValue = "PASS"
-#  scriptTimeout = 5
-#  replayEnabled = false
-#  replayTimeout = 0
-
-
-`gcloud compute instance-groups managed set-autoscaling --min-num-replicas=3 --max-num-replicas=3 --cool-down-period=1200 --target-cpu-utilization=0.8 --zone=<REGION>-b <STACK NAME>-igm`
-echo "PASS"
