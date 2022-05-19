03606d2b7433edfa776a5e5d5150e3a98a5e0435
shimkus <m.shimkus@f5.com>
2022-05-19 23:12:57 +0000

    Release commited to v3.17.0 tag

--- automated-test-scripts/f5-google-gdm-templates/common/verify_create.sh
+++
@@ -1,7 +0,0 @@
-#  expectValue = "COMPLETED"
-#  expectFailValue = "FAILED"
-#  scriptTimeout = 2
-#  replayEnabled = true
-#  replayTimeout = 30
-
-gcloud deployment-manager deployments describe <STACK NAME>
