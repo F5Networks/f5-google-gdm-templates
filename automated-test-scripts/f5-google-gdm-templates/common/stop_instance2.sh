5b00c14c513bc3be397f68dd91a5a4d095ee6387
shimkus <m.shimkus@f5.com>
2022-10-04 17:33:00 +0000

    Release commited to v4.1.0 tag

--- automated-test-scripts/f5-google-gdm-templates/common/stop_instance2.sh
+++
@@ -1,9 +0,0 @@
-#  expectValue = "Stopping instance"
-#  scriptTimeout = 10
-#  replayEnabled = false
-#  replayTimeout = 0
-
-# stop instance - required in certain cases such as failover which won't delete if
-# forwarding rules are not on the original box
-
-gcloud compute instances stop bigip2-<STACK NAME> --zone=<AVAILABILITY ZONE2>
