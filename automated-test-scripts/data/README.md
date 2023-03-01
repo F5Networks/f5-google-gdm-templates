cf69e1eea7c0c6b87e74f2487375a847192fa94b
shimkus <m.shimkus@f5.com>
2023-03-01 22:23:10 +0000

    Release commited to v4.3.0 tag

--- automated-test-scripts/data/README.md
+++
@@ -1,2 +0,0 @@
-This directory contains input parameters files that are ingested by dewdrop.py.
-Passed via '-i example-file.yaml' when running dewdrop.  
--- automated-test-scripts/f5-google-gdm-templates/common/start_instance2.sh
+++
@@ -1,9 +0,0 @@
-#  expectValue = "Starting instance"
-#  scriptTimeout = 10
-#  replayEnabled = false
-#  replayTimeout = 0
-
-# start instance - required to grab logs in cases 
-# where big-ip was shutdown during test.
-
-gcloud compute instances start bigip2-<STACK NAME> --zone=<AVAILABILITY ZONE>
